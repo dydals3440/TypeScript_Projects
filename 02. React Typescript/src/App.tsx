13bf443999ada784aac126bf86b901af4e9e4564
김용민 <dydals3440@gmail.com>
2023-10-06 03:14:02 +0900

    feat: useRef 타입

--- 02. React Typescript/src/App.tsx
+++ 02. React Typescript/src/App.tsx
@@ -1,10 +1,12 @@
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   const todos = [{ id: 't1', text: 'Finish the course' }];
   return (
     <div className='App'>
-      <TodoList items={todos!} />
+      <NewTodo />
+      <TodoList items={todos} />
     </div>
   );
 };
