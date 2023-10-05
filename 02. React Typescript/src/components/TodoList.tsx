03a227c1d4db8fc477ab5519932fbc4fe98824d6
김용민 <dydals3440@gmail.com>
2023-10-06 04:36:28 +0900

    styles: Todo Styling

--- 02. React Typescript/src/components/TodoList.tsx
+++ 02. React Typescript/src/components/TodoList.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './TodoList.css';
+
 // Props의 타입도 정의해줘야함(부모 요소에서 받아오는 데이터)
 interface TodoListProps {
   items: { id: string; text: string }[];
@@ -13,6 +15,7 @@ const TodoList: React.FC<TodoListProps> = (props) => {
         return (
           <li key={todo.id}>
             <span>{todo.text}</span>
+            {/* onClick 버튼이 실행되면, 인터페이스의 onDeleTodo 프로퍼티를 받아서 함수가 호출. 이 방법의 문제는 todoID를 매개변수로 받는다는 것  이 방법이 제대로 실행되려면 bind(null, onDeletodo에서 받은 매개변수)*/}
             <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
               Delete
             </button>
