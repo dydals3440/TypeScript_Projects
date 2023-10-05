87351af6c4e9174b6fd3ebb0883aa8bb73e3a3bf
김용민 <dydals3440@gmail.com>
2023-10-05 22:36:53 +0900

    feat: type casting 해결

--- 01. Google Map/src/app.ts
+++ 01. Google Map/src/app.ts
@@ -1,1 +1,13 @@
-// Code goes here!
+// form이 null이 아니라는 것을 확실하게 하기 위해서 ! 타입 붙여줌
+const form = document.querySelector('form')!;
+// Type Casting (value access 해결!)
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  // addressInput.value가 무엇을 갖고오는지 모르기떄문에 Type Error 발생
+  const enteredAddress = addressInput.value;
+  // send this to Google's API
+}
+
+form.addEventListener('submit', searchAddressHandler);
