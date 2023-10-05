8f83ee777efcfafddb391d5e79536a42df6c671d
김용민 <dydals3440@gmail.com>
2023-10-06 01:45:19 +0900

    feat: api

--- 01. Google Map/src/app.ts
+++ 01. Google Map/src/app.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "AIzaSyBMUBvHgQqtDmrteQpJAHuCViaVcjwLs8s'";
+const GOOGLE_API_KEY = "API코드'";
 
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
