ba75ae12b37693db19e213132c2b70f35b25ef06
김용민 <dydals3440@gmail.com>
2023-10-06 03:25:57 +0900

    feat Cross Component Comunication

--- 02. React Typescript/src/App.tsx
+++ 02. React Typescript/src/App.tsx
@@ -2,10 +2,16 @@ import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
+  // 리액트에서는, 배열을 업데이트 하는 것만으로 충분하지 않음.
   const todos = [{ id: 't1', text: 'Finish the course' }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
   return (
     <div className='App'>
-      <NewTodo />
+      {/* 함수를 실행시키는게 아닌, 함수를 가리키는 포인터를 onAddTodo에 전달! */}
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
--- 02. React Typescript/src/components/NewTodo.tsx
+++ 02. React Typescript/src/components/NewTodo.tsx
@@ -1,6 +1,10 @@
 import React, { useRef } from 'react';
 
-const NewTodo: React.FC = () => {
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
   // useRef는 제네릭 타입임, 어떤 타입을 전달해야하나면, ref에는, input요소를 지정했기에 HTML InputElement 저장, 그 후 ref를 설정할 때 기본값을 입력해야하는데, 이건 null이다. 컴포넌트는 코드가 실행될떄 처음으로 렌더링되고, 아직 렌더링되지 않았기에 가리키는 값이 없음. 렌더링 후 연결 설정되야 ref이 동작
   const textInputRef = useRef<HTMLInputElement>(null);
   const todoSubmitHandler = (event: React.FormEvent) => {
@@ -8,6 +12,7 @@ const NewTodo: React.FC = () => {
     // 타입스크립트는 textInputRef가 제대로 연결되었는지 확신할 수 없기 때문에, null일수도 있다는 에러가 발생, !을 붙여 해결해준다.
     const enteredText = textInputRef.current!.value;
     console.log(enteredText);
+    props.onAddTodo(enteredText);
   };
 
   return (
